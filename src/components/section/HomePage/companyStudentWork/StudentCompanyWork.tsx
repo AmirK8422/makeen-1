56e24346149c92b00ffe4178d48c1c263f68ba8c
Amir <amirnekoonam85@gmail.com.>
2025-09-22 16:07:17 +0330

    fixed-company-icons

--- src/components/section/HomePage/companyStudentWork/StudentCompanyWork.tsx
+++ src/components/section/HomePage/companyStudentWork/StudentCompanyWork.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 function StudentCompanyWork() {
   return (
-    <div className=' mx-20 mt-28 flex flex-col items-center justify-center'>
+    <div className=' mx-20 my-28 flex flex-col items-center justify-center'>
           <h2 className='font-semibold text-3xl pb-4'>با افتخار</h2>
           <p className='text-16 md:text-2xl mb-6 '>دانشجو‌های مکین در شرکت های زیر مشغول به کارند</p>
           <img src="img/icons/companyIcons.png" alt="companyLogo" className='w-full' />
