2248a83e1dcaadd9fbfe0bdb4e9032636dc5ac93
Mohammad Hasan <mohamadhasan.m.d.n@gmail.com>
2025-10-06 15:30:45 +0330

    first commit in sections branch

--- src/app/Admin/(URL)/Sections/CoWork/page.tsx
+++ src/app/Admin/(URL)/Sections/CoWork/page.tsx
@@ -3,11 +3,11 @@ import React from 'react'
 function CoWork() {
   return (
     <div className='bg-white p-4 rounded-[8px] m-4'>
-         <div className='border-1 border-dashed h-[202px] w-[240px] rounded-[8px] flex justify-center items-center text-[#8E8E93] text-[16px] font-medium'>
+         <div className='border-1 border-dashed h-[202px] w-[240px] rounded-[8px] flex justify-center items-center text-[#8E8E93] text-[16px] font-medium mb-[60px]'>
                 افزودن عکس +
          </div>
          <div className='flex flex-col'>
-            <h1>متن سکشن</h1>
+            <h1 className='font-medium text-[18px]'>متن سکشن</h1>
          </div>
     </div>
   )
